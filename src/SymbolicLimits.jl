742e5de0d76323ca1423417fa92c77c38bdce8c2
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-03-20 11:15:25 -0500

    fix typo in docstring

--- src/SymbolicLimits.jl
+++ src/SymbolicLimits.jl
@@ -11,7 +11,7 @@ const _AUTO = :__0x6246e6c6ad56df8113c7eb80b2a84080__
 Compute the limit of `expr` as `var` approaches `h` and return `(limit, assumptions)`. If
 all the `assumptions` are true, then the returned `limit` is correct.
 
-`side` indicates the direction from which `var`` approaches `h`. It may be one of `:left`,
+`side` indicates the direction from which `var` approaches `h`. It may be one of `:left`,
 `:right`, or `:both`. If `side` is `:both` and the two sides do not align, an error is
 thrown. Side defaults to `:both` for finite `h`, `:left` for `h = Inf`, and `:right` for
 `h = -Inf`.
