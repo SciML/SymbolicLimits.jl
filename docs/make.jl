aeb0a5aeaae64455ca3e96f1156408b9eab9ef04
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-09-11 08:17:08 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,9 @@ DocMeta.setdocmeta!(SymbolicLimits, :DocTestSetup, :(using SymbolicLimits); recu
 makedocs(;
     modules = [SymbolicLimits],
     authors = "Lilith Orion Hafner <lilithhafner@gmail.com> and contributors",
-    repo = "https://github.com/LilithHafner/SymbolicLimits.jl/blob/{commit}{path}#{line}",
     sitename = "SymbolicLimits.jl",
     format = Documenter.HTML(;
         prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://LilithHafner.github.io/SymbolicLimits.jl",
         edit_link = "main",
         assets = String[]
     ),
@@ -20,6 +18,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo = "github.com/LilithHafner/SymbolicLimits.jl",
+    repo = "github.com/SciML/SymbolicLimits.jl",
     devbranch = "main"
 )
